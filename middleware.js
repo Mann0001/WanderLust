09ebb75ed0170aaae88c8f724e0a36d77a8631d4
Mann <manmeetms97@gmail.com>
2025-07-25 15:23:10 +0530

    added an admin

--- middleware.js
+++ middleware.js
@@ -22,8 +22,11 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
-  if (!listing.owner.equals(res.locals.currUser._id)) {
-    req.flash("error", "You are not the owner of this listing");
+  if (
+    !listing.owner.equals(res.locals.currUser._id) &&
+    !res.locals.currUser.isAdmin
+  ) {
+    req.flash("error", "You are not authorized to perform this action");
     return res.redirect(`/listings/${id}`);
   }
   next();
@@ -52,8 +55,11 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
   let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
-  if (!review.author.equals(res.locals.currUser._id)) {
-    req.flash("error", "You are not the owner of this listing");
+  if (
+    !review.author.equals(res.locals.currUser._id) &&
+    !res.locals.currUser.isAdmin
+  ) {
+    req.flash("error", "You are not authorized to delete this review");
     return res.redirect(`/listings/${id}`);
   }
   next();
